13dfc18b7bda19372f380071410544bf5226d51d
Ying Geng <33822891+yeti46g@users.noreply.github.com>
2020-05-15 12:40:27 -0500

    added docstring

--- app/run.py
+++ app/run.py
@@ -15,6 +15,14 @@ from sqlalchemy import create_engine
 app = Flask(__name__)
 
 def tokenize(text):
+    """Function to tokenize user input messages
+
+    Input:
+        text
+
+    Returns:
+        clean tokens
+    """
     tokens = word_tokenize(text)
     lemmatizer = WordNetLemmatizer()
 
@@ -37,14 +45,24 @@ model = joblib.load("models/classifier.pkl")
 @app.route('/')
 @app.route('/index')
 def index():
-    
+    """Function to render index page of web app
+        with visualizations plotted with plotly
+
+    Input:
+        None
+
+    Returns:
+        render index page of web app and show two visualizations
+
+    """
+
     # extract data needed for visuals
     # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
     category_counts = df.sum()[3:]
     category_names = list(df.sum()[3:].index)
-    
+
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
     graphs = [
@@ -74,7 +92,7 @@ def index():
                     y=category_counts
                 )
             ],
-            
+
             'layout': {
                 'title': 'Distribution of Message Genres',
                 'yaxis': {
@@ -84,14 +102,14 @@ def index():
                     'title': "Genre"
                 }
             }
-                
+
         }
     ]
-    
+
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
-    
+
     # render web page with plotly graphs
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
 
@@ -99,14 +117,22 @@ def index():
 # web page that handles user query and displays model results
 @app.route('/go')
 def go():
+    """Function to take user input and call saved ML model to classify the message
+
+    Input:
+        None
+
+    Returns:
+        render the page that shows classification result
+    """
     # save user input in query
-    query = request.args.get('query', '') 
+    query = request.args.get('query', '')
 
     # use model to predict classification for query
     classification_labels = model.predict([query])[0]
     classification_results = dict(zip(df.columns[4:], classification_labels))
 
-    # This will render the go.html Please see that file. 
+    # This will render the go.html Please see that file.
     return render_template(
         'go.html',
         query=query,
